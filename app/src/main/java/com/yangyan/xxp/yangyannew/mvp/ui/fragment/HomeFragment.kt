cc26b519e046aff4735f2b7052e47960c9be590c
zhongwenpeng <1340751953@qq.com>
2018-06-14 17:42:56 +0800

    [MOD] 减少 oom可能

--- app/src/main/java/com/yangyan/xxp/yangyannew/mvp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/yangyan/xxp/yangyannew/mvp/ui/fragment/HomeFragment.kt
@@ -7,10 +7,13 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.ScrollView
+import com.bumptech.glide.Glide
 import com.jess.arms.base.BaseFragment
 import com.jess.arms.di.component.AppComponent
 import com.yangyan.xxp.yangyannew.R
@@ -147,6 +150,22 @@ class HomeFragment : BaseFragment<HomePresenter>(), HomeContract.View,
 
     private fun initRecyclerView() {
         mRvHome.layoutManager = mLayoutManager
+        mRvHome.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && Glide.with(this@HomeFragment).isPaused) {
+                    Glide.with(this@HomeFragment).resumeRequests()
+                } else {
+                    Glide.with(this@HomeFragment).pauseRequests()
+                }
+
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+            }
+        })
         mRvHome.adapter = mAdapter.apply {
             setOnItemClickListener { view, viewType, position ->
                 kotlin.run {
